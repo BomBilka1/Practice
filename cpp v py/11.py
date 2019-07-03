3f3b9aa8a201d2d6b289d99e6064c43a5740c801
BomBilka! <45636236+BomBilka1@users.noreply.github.com>
2019-07-03 11:43:28 +0500

    Create 11.py

---
+++ cpp v py/11.py
@@ -0,0 +1,28 @@
+import math
+import random
+n=int(input("Введте размерность масива: "))
+arr=[]
+for i in range(n):
+    arr.append(random.randint(-10,10))
+print(arr)
+kol=0
+mins=arr[0]
+sums=0
+for i in range(n):
+    if arr[i]>=0:
+        kol+=1
+print(kol)
+for i in range(n):
+    if arr[i]==0:
+        mins = arr[i];
+        sums = 0
+    else:
+        sums += arr[i]
+print(sums)
+for i in range(n):
+    for j in range(n):
+        if arr[i]<arr[j]:
+            b=arr[j]
+            arr[j]=arr[i];
+            arr[i]=b;
+print(arr)
